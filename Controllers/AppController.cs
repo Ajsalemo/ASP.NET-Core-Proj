664dbb81a8e503eb230f424084fca6c8d4f9faad
Anthony Salemo <ajssalemo@gmail.com>
2021-04-11 21:12:02 -0400

    added some logic to change the buildId depending on if the app is running in dev or prod

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace ASP.NET_Core_Proj.Controllers
 {
@@ -14,17 +16,30 @@ namespace ASP.NET_Core_Proj.Controllers
     public class AppController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
         public string runtime = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+        public string buildId;
 
-        public AppController(IConfiguration configuration)
+        public AppController(IConfiguration configuration, IWebHostEnvironment env)
         {
             _configuration = configuration;
+            _env = env;
         }
-        
+
+
         [HttpGet]
         public String Index()
         {
-            var buildId = _configuration["CodeVersion:Number"];
+            // If running locally then show the below message instead of the not-yet-replaced token value in appsettings.json
+            // If running in production then display the actual DevOps buildId
+            if (_env.IsDevelopment())
+            {
+                buildId = "Local Development - v1.0";
+            }
+            else
+            {
+                buildId = _configuration["CodeVersion:Number"];
+            }
             var message = "ASP.NET-Core-Proj " + "| Runtime: " + runtime + " | Build Version: " + buildId;
             return message;
         }
